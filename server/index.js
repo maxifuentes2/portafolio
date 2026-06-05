d810f44ba0c60baa11ee77e3b5abc01fc8f76509
maxifuentes2 <maximofuentes022@gmail.com>
2026-06-05 02:56:05 -0300

    fix: add SMTP timeouts to prevent server hang

--- server/index.js
+++ server/index.js
@@ -13,6 +13,9 @@ const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: Number(process.env.SMTP_PORT),
     secure: false,
+    connectionTimeout: 5000,
+    greetingTimeout: 5000,
+    socketTimeout: 10000,
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
